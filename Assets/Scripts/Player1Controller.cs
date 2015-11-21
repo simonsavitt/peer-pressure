f54146a342c5226c1cb095b3bd393a707a944b44
Paul Fielek <paulfielek@gmail.com>
2015-11-21 13:10:51 -0500

    players moving

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -3,15 +3,54 @@ using System.Collections;
 
 public class Player1Controller : MonoBehaviour {
 
+	public int alpha = 5;
+	public int beta = 5;
+
+	public float speed = 5;
+	public float attackSpeed = 1;
+
+	/* ATTRIBUTES
+	 * 
+	 * movement speed +a
+	 * action frequency +a
+	 * action speed (duration until attack lands) +b
+	 * action range +b
+	 * fear of opponent +b
+	 * rage towards opponent +a
+	 * chance of attack landing +a
+	 * availability of actions +b
+	 * choice of actions
+	 */
+	
+	/* ACTIONS
+	 * 
+	 * Attack
+	 * Intimidate +a (lowers attack frequency)
+	 * Taunt +b (lowers defense)
+	 * Defend +b (raises defense)
+	 * Bully +a (lowers defense)
+	 * Appreciate +b (lowers attack)
+	 * Cheat +a (affects environment)
+	 * Run +b (runs from opponent)
+	 */
+
+	/* WEAPONS
+	 * 
+	 * whiffle ball bat 
+	 * pillow
+	 */
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey ("up")) {
+		GameObject player2 = GameObject.FindGameObjectWithTag ("Player2");
+		Transform player2Transform = player2.transform;
+		transform.position = Vector2.MoveTowards (transform.position, player2Transform.position, speed * Time.deltaTime);
 
-		}
+//		float nextAttack = Time + 
 	}
 }
--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player2Script : MonoBehaviour {
+public class Player2Controller : MonoBehaviour {
+
+	public int alpha = 5;
+	public int beta = 5;
+
+	public float speed = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +15,8 @@ public class Player2Script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		GameObject player1 = GameObject.FindGameObjectWithTag ("Player1");
+		Transform player1Transform = player1.transform;
+		transform.position = Vector2.MoveTowards (transform.position, player1Transform.position, speed * Time.deltaTime);
 	}
 }
